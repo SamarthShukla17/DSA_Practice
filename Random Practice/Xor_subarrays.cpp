8cf7e13b114ba8f97eb22e128e3488f84067cc68
Sam <paarthsamarth@gmail.com>
2025-05-19 14:14:00 +0530

    Xor with better approaches

--- Random Practice/Xor_subarrays.cpp
+++ Random Practice/Xor_subarrays.cpp
@@ -23,6 +23,52 @@ using namespace std;
 // }
 
 
+
+//Better approach
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int subarraysWithXorK(vector<int> a, int k) {
+//     int n = a.size();
+//     int cnt = 0;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         int xorr = 0;
+//         for (int j = i; j < i; j++)
+//         {
+//             xorr = xorr ^ a[j];
+//             if (xorr == k) cnt++;
+//         }
+        
+//     }
+//     return cnt;
+    
+// }
+
+
+//Optimal approach
+#include <bits/stdc++.h>
+using namespace std;
+
+int subarraysWithXorK(vector<int> a, int k) {
+    int n = a.size();
+    int xr = 0;
+    map<int, int> mpp;
+    mpp[xr]++;
+    int cnt = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        xr = xr ^ a[i];
+        int x = xr ^ k;
+        cnt += mpp[x];
+        mpp[xr]++;
+    }
+    return cnt;
+}
+
+
 int main()
 {
     vector<int> a = {4, 2, 2, 6, 4};
